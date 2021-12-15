35ace8af07028093096b4d5c509d558bc7237d0c
Michael Fitzmayer <mail@michael-fitzmayer.de>
2021-12-15 09:39:28 +0100

    Reorganise toolchain, add option to install files directly on the N-Gage (if connected via USB)

--- cmake/nokia-ngage-toolchain.cmake
+++ cmake/nokia-ngage-toolchain.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
+set(SRC_DIR            ${CMAKE_SOURCE_DIR}/src)
 set(RIFTS_DIR          ${CMAKE_SOURCE_DIR}/Rifts)
 set(SDK_ROOT           ${CMAKE_SOURCE_DIR}/Symbian/6.1)
 set(EPOC_PLATFORM      ${SDK_ROOT}/Shared/EPOC32)
@@ -16,41 +17,19 @@ set(CMAKE_CXX_COMPILER_ID_RUN TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
 set(CMAKE_CXX_COMPILER_WORKS  TRUE)
 
-set(LIBS_UREL
-    ${EPOC_LIB}/edllstub.lib
-    ${EPOC_LIB}/egcc.lib
-    ${EPOC_LIB}/ws32.lib
-    ${RIFTS_DIR}/Prototype/NokiaRasterizer/App32/LIB/UREL/NRENDERER.LIB
-    ${EPOC_LIB}/bitgdi.lib
-    ${EPOC_LIB}/euser.lib
-    ${EPOC_LIB}/apparc.lib
-    ${EPOC_LIB}/cone.lib
-    ${EPOC_LIB}/eikcore.lib
-    ${EPOC_LIB}/eikcoctl.lib
-    ${EPOC_LIB}/avkon.lib
-    ${EPOC_LIB}/fbscli.lib
-    ${EPOC_LIB}/efsrv.lib
-    ${EPOC_LIB}/estlib.lib
-    ${EPOC_LIB}/mediaclientaudiostream.lib
-    ${EPOC_LIB}/hal.lib
-#    ${EPOC_LIB}/bluetooth.lib
-#    ${EPOC_LIB}/esock.lib
-    ${EPOC_LIB}/flogger.lib
-    ${EPOC_LIB}/sysagt.lib
-    ${EPOC_LIB}/msgs.lib)
-
 include_directories(
     SYSTEM
     ${EPOC_PLATFORM}/include
     ${SDK_ROOT}/Series60/Epoc32/Include
     ${SDK_ROOT}/Series60/Epoc32/Include/libc
-    ${RIFTS_DIR}/Prototype/NokiaRasterizer/App32/LIB/INC)
+    ${RIFTS_DIR}/Prototype/NokiaRasterizer/App32/LIB/INC
+    ${SRC_DIR})
 
 set(CORE_FLAGS "-s -fomit-frame-pointer -O -march=armv4t -mthumb-interwork -pipe -nostdinc -Wall -Wno-ctor-dtor-privacy -Wno-unknown-pragmas -Wno-switch -mstructure-size-boundary=8")
 
 set(CMAKE_CXX_FLAGS ${CORE_FLAGS} CACHE INTERNAL "cxx compiler flags")
 
-macro(build_dll source uid1 uid2 uid3)
+function(build_dll machine source file_ext uid1 uid2 uid3)
     # Create new DefFile from in library
     add_custom_target(
         ${source}.def
@@ -58,25 +37,25 @@ macro(build_dll source uid1 uid2 uid3)
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/dlltool -m arm_interwork --output-def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a)
+        ${EPOC_PLATFORM}/gcc/bin/dlltool -m ${machine} --output-def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a)
 
     # Create new Export file from generated DefFle
     add_custom_target(
-        tmp.exp
+        ${source}_tmp.exp
         ALL
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/${source}.def
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/dlltool -m arm_interwork --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --output-exp ${CMAKE_CURRENT_BINARY_DIR}/tmp.exp --dllname ${source}[${UID3}].app)
+        ${EPOC_PLATFORM}/gcc/bin/dlltool -m ${machine} --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --output-exp ${CMAKE_CURRENT_BINARY_DIR}/${source}_tmp.exp --dllname ${source}[${uid3}].${file_ext})
 
     # Create new Base file
     add_custom_target(
         ${source}.bas
         ALL
         DEPENDS
-        ${CMAKE_CURRENT_BINARY_DIR}/tmp.exp
+        ${CMAKE_CURRENT_BINARY_DIR}/${source}_tmp.exp
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/ld -s -e _E32Dll -u _E32Dll ${CMAKE_CURRENT_BINARY_DIR}/tmp.exp --dll --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas -o ${CMAKE_CURRENT_BINARY_DIR}/tmp.app ${EPOC_LIB}/edll.lib --whole-archive ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a --no-whole-archive ${LIBS_UREL})
+        ${EPOC_PLATFORM}/gcc/bin/ld -s -e _E32Dll -u _E32Dll ${CMAKE_CURRENT_BINARY_DIR}/${source}_tmp.exp --dll --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas -o ${CMAKE_CURRENT_BINARY_DIR}/${source}_tmp.${file_ext} ${EPOC_LIB}/edll.lib --whole-archive ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a --no-whole-archive ${ARGN})
 
     # Create new EXPORT file with def a
     add_custom_target(
@@ -85,7 +64,7 @@ macro(build_dll source uid1 uid2 uid3)
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/dlltool -m arm_interwork --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --dllname ${source}[${UID3}].app --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas --output-exp ${CMAKE_CURRENT_BINARY_DIR}/${source}.exp)
+        ${EPOC_PLATFORM}/gcc/bin/dlltool -m ${machine} --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --dllname ${source}[${uid3}].${file_ext} --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas --output-exp ${CMAKE_CURRENT_BINARY_DIR}/${source}.exp)
 
     # Create new interface LIB file with def a
     add_custom_target(
@@ -94,7 +73,7 @@ macro(build_dll source uid1 uid2 uid3)
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/${source}.exp
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/dlltool -m arm_interwork --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --dllname ${source}[${UID3}].app --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas --output-lib ${CMAKE_CURRENT_BINARY_DIR}/${source}.lib)
+        ${EPOC_PLATFORM}/gcc/bin/dlltool -m ${machine} --def ${CMAKE_CURRENT_BINARY_DIR}/${source}.def --dllname ${source}[${uid3}].${file_ext} --base-file ${CMAKE_CURRENT_BINARY_DIR}/${source}.bas --output-lib ${CMAKE_CURRENT_BINARY_DIR}/${source}.lib)
 
     add_custom_target(
         ${source}.map
@@ -102,13 +81,25 @@ macro(build_dll source uid1 uid2 uid3)
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/${source}.lib
         COMMAND
-        ${EPOC_PLATFORM}/gcc/bin/ld -s -e _E32Dll -u _E32Dll --dll ${CMAKE_CURRENT_BINARY_DIR}/${source}.exp -Map ${CMAKE_CURRENT_BINARY_DIR}/${source}.map -o tmp.app ${EPOC_LIB}/edll.lib --whole-archive ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a --no-whole-archive ${LIBS_UREL})
+        ${EPOC_PLATFORM}/gcc/bin/ld -s -e _E32Dll -u _E32Dll --dll ${CMAKE_CURRENT_BINARY_DIR}/${source}.exp -Map ${CMAKE_CURRENT_BINARY_DIR}/${source}.map -o ${source}_tmp.${file_ext} ${EPOC_LIB}/edll.lib --whole-archive ${CMAKE_CURRENT_BINARY_DIR}/lib${source}.a --no-whole-archive ${ARGN})
 
     add_custom_target(
-        ${source}.app
+        ${source}.${file_ext}
         ALL
         DEPENDS
         ${CMAKE_CURRENT_BINARY_DIR}/${source}.map
         COMMAND
-        ${EPOC_PLATFORM}/Tools/petran ${CMAKE_CURRENT_BINARY_DIR}/tmp.app ${CMAKE_CURRENT_BINARY_DIR}/${source}.app -nocall -uid1 ${UID1} -uid2 ${UID2} -uid3 ${UID3})
-endmacro()
+        ${EPOC_PLATFORM}/Tools/petran ${CMAKE_CURRENT_BINARY_DIR}/${source}_tmp.${file_ext} ${CMAKE_CURRENT_BINARY_DIR}/${source}.${file_ext} -nocall -uid1 ${uid1} -uid2 ${uid2} -uid3 ${uid3})
+endfunction()
+
+function(install_file project_name source_dir file drive_letter)
+    add_custom_target(
+        ${file}_install
+        ALL
+        DEPENDS
+        ${CMAKE_CURRENT_BINARY_DIR}/${file}
+        COMMAND
+        ${CMAKE_COMMAND} -E copy
+        ${CMAKE_CURRENT_BINARY_DIR}/${file}
+        ${drive_letter}:/system/apps/${project_name}/${file})
+endfunction()
